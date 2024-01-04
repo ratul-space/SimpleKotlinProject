ddf544ad52facbd763af83816a04dc2f80c6aced
ratul-space <to.ratul.hosen@gmail.com>
2024-01-04 14:59:34 +0600

    feat: fix roted

--- app/src/main/java/com/example/simpleKotlinProject/MainActivity.kt
+++ app/src/main/java/com/example/simpleKotlinProject/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.simpleKotlinProject
 
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
@@ -13,6 +15,7 @@ class MainActivity : AppCompatActivity() {
     companion object {
         val TAG: String = "MainActivity"
     }
+
     private var mLastNames: ListView? = null
     private var mArrayList: ArrayList<String>? = null
 
@@ -26,26 +29,48 @@ class MainActivity : AppCompatActivity() {
             mArrayList = ArrayList<String>()
 
         val etName = findViewById<EditText>(R.id.etName)
-        val lstNames = findViewById<ListView>(R.id.lstNames)
-
-        val arrayList = ArrayList<String>()
-
         findViewById<Button>(R.id.btnAdd).setOnClickListener {
-
-//            Log.d(TAG, "onCreate: findViewById<Button>(R.id.btnAdd).setOnClickListener: ${edtname.text.toString()} ")
-
             val name = etName.text.toString()
 
+
+            val lstNames = findViewById<ListView>(R.id.lstNames)
+
+            val arrayList = ArrayList<String>()
+
             if (name.isNullOrBlank()) {
-                arrayList.add(name)
-                lstNames.adaptar = ArrayAdapter<String>(
+                mArrayList?.add(name)
+                mLastNames?.adapter = ArrayAdapter<String>(
                     applicationContext,
                     android.R.layout.simple_list_item_1,
-                    arrayList
+                    mArrayList
                 )
-
-                etName.setText("as;dofijasdofijaosdifj")
+                etName.setText("")
             }
         }
+        Log.d(TAG, "onCreate: ends")
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        Log.d(TAG, "onSaveInstanceState: starts")
+        super.onSaveInstanceState(outState)
+
+        outState.putStringArrayList(STATE_ARRAY_LIST, mArrayList)
+        Log.d(TAG, "onSaveInstanceState: ends")
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        Log.d(TAG, "onRestoreInstanceState: starts")
+        super.onRestoreInstanceState(savedInstanceState)
+
+        mArrayList = savedInstanceState?.getStringArrayList(STATE_ARRAY_LIST)
+        mLastNames?.adapter = ArrayAdapter<String>(
+            applicationContext,
+            android.R.layout.simple_list_item_1,
+            mArrayList
+        )
+
+        super.onRestoreInstanceState(savedInstanceState)
+        Log.d(TAG, "onRestoreInstanceState: ends")
     }
 }
